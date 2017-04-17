e7fffa4e89e960716d5b194d090244572a31e76a
Your Name <you@example.com>
2017-04-17 16:09:37 +0800

    到连接了数据库部分

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
-module.exports =  function(req, res) {
-  res.send('hello, candy');
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts');
+  });
+  app.use('/signup', require('./signup'));
+  app.use('/signin', require('./signin'));
+  app.use('/signout', require('./signout'));
+  app.use('/posts', require('./posts'));
 };
